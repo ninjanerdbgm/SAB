8c38036dc319c05575c8ff387021be34b3a95a89
ninjanerdbgm <ninjanerd2781@gmail.com>
2014-08-13 04:48:22 +0000

    WARNING: Messy Code

--- SnooBot.py
+++ SnooBot.py
@@ -1,6 +1,7 @@
 import time
 import praw
 import sys, time
+import os
 
 """ Enable or diable debug output """
 DEBUG = 0
@@ -19,14 +20,23 @@ Seriously.  This thing is like super-alpha right now.
 Idle in chat and follow some basic commands to get stuff from reddit.
 """
 
-print("SnooBot v 0.0000000000000000001." \
-        "\n\nSearching for posts on /r/askreddit")
+print("SnooBot v 0.0000000000000000001.")
 
 r = praw.Reddit('AskReddit [Stories] Aggregator Bot, v 0.00000000001'
                 'Seriously.  This thing is in, like, super alpha.'
                 'by /u/ninjanerdbgm')
 
-r.login('StoryAggregatorBot', 'ishouldprobablychangethis1')
+print("Attempting to connect to reddit...")
+loggedin=0
+while loggedin==0:
+	try:
+		r.login('StoryAggregatorBot', 'ishouldprobablychangethis1')
+		loggedin=1
+	except:
+		print("Unable to connect.  Reddit might be down.  Trying again in a few minutes...")
+		time.sleep(300)
+print("Done!")
+
 already_done = []
 skip_posts = []
 
@@ -41,8 +51,9 @@ First, let's make a list of some of the common ways to write the tag
 storytags = ['[stories]', '[story]', '[ stories]', '[stories ]', '[ stories ]', 'stories only']
 
 while True:
+	print("Searching /r/askreddit for [Story] posts...")
         subreddit = r.get_subreddit('askreddit')
-        for submission in subreddit.get_hot(limit=150):
+        for submission in subreddit.get_hot(limit=500):
 #	    try:
                 """
                 Find the [Stories] tag
@@ -77,21 +88,36 @@ while True:
 							print("        | DEBUG: Comment length: {0}".format(len(comment.body)))
 						if len(comment.body) > 250 and comment.id not in already_covered:
 	                                                author = comment.author
-	                                                if author.name == "StoryAggregatorBot":
-	                                                        edit_comments.append(comment)
-								madepost = 1
-							else:
-		                                                a=0
-		                                                comlink=""
-		                                                while a < 80:
-		                                                        comlink+=(chr(ord(comment.body[a])))
-		                                                        a+=1
-		                                                url = submission.permalink
-		                                                url += comment.id
-		                                                comlink += "..."
-		                                                makeaggregate += "\n[{0}]({1}) | /u/{2}".format(comlink, url, author.name)
-								print("        | [{0}]({1}), by {2}".format(comlink, url, author.name))
-	        	                                        already_covered.add(comment.id)
+							if author.name <> "xAutoModeratorx":
+		                                                """ Let's check if the comment we found was the bot's own comment.
+		                                                    If it was, then we'll make a note to edit it later and skip it as
+		                                                    part of the aggregated posts.
+		                                                """
+		                                                if author.name == "StoryAggregatorBot":
+		                                                        edit_comments.append(comment)
+									madepost = 1
+								else:
+			                                                a=0
+			                                                comlink=""
+			                                                while a < 80:
+										try: 
+											if chr(ord(comment.body[a])) == chr(13) or chr(ord(comment.body[a])) == chr(10):
+												""" Let's replace line feeds with spaces to keep the format """
+												comlink+=" "
+											else:
+					                                                        comlink+=(chr(ord(comment.body[a])))
+					                                                a+=1
+										except:
+											print("        | (((Encountered a strange encoding.  Let's replace it with null.)))")
+											comlink+=""
+											a+=1
+			                                                url = submission.permalink
+			                                                url += comment.id
+			                                                comlink += "..."
+			                                                makeaggregate += "\n[{0}]({1}) | /u/{2}".format(comlink, url, author.name)
+									print("        | [{0}]({1}), by {2}".format(comlink, url, author.name))
+									time.sleep(0.05)
+		        	                                        already_covered.add(comment.id)
 	                                makeaggregate += "\n\nPlease downvote this bot if you find it annoying and it will delete the post automatically.\
 							\n\n[^Send ^message ^to ^creator](http://www.reddit.com/message/compose/?to=ninjanerdbgm) ^| [^What ^is ^a ^bot?](http://www.reddit.com/r/botwatch/wiki/faq) ^|\
 							[^Did ^this ^bot ^mess ^up?  ^Let ^me ^know!](http://www.reddit.com/message/compose/?to=ninjanerdbgm) ^| [^Source ^Code](https://github.com/ninjanerdbgm/SAB)"
@@ -109,11 +135,14 @@ while True:
 	                                else:
 						for comments in edit_comments:
 							comments.edit(makeaggregate)
+							edit_comments.remove(comments)
 							print("        | Successfully edited a message!")
 	                        else:
 	                                print("        | At {0} comments, this post isn't popular enough for a submission, yet.".format(numcoms))
 #            except:
 #			print("Evidently, something went wrong while trying to search for posts.  Reddit may be down for maintainence.  Let's just take a nap for a while and try again later.")
+	print("----------------------------------------------------------------------")
+	print("Couldn't find anymore reddit posts for the time being.  Let's move on!")
         """ Look for unpopular posts """
         print("Looking for unpopular bot posts...")
         user = r.get_redditor("StoryAggregatorBot")
@@ -126,5 +155,16 @@ while True:
 					skip_posts.append(i.submission.id)
                 		        i.delete()
         """ ======================== """
-        time.sleep(600)
+	print("Done!")
+	print("----------------------------------------------------------------------")
+	time.sleep(1)
+	i=600
+	secs=0
+	while i > 0:
+#		os.system('clear')
+		sys.stdout.write("This round of scouring is over!  {0:0>2}:{1:0>2} until the next round!\r".format((i/60),(i%60)))
+		sys.stdout.flush()
+		time.sleep(1)
+		i-=1
+        print("-------------------------------------------")	
 
